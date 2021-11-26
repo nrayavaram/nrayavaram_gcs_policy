d1189e34a1b748a7d6230b20601ed74b5d38b894
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-26 11:19:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,10 +31,10 @@ resource "google_storage_bucket" "GCS" {
   }
 
  labels = {
-    owner = ["wf","hybridenv"] 
-    application_division = ["pci", "paa", "hdpa", "hra", "others"]
-    application_name = ["app1","demo"]
-    application_role = ["app", "web", "auth", "data"]
+    owner = "wf","hybridenv" 
+    application_division = "pci", "paa", "hdpa", "hra", "others"
+    application_name = "app1","demo"
+    application_role = "app", "web", "auth", "data"
     au = "0223092"
     prefix = "us-"
   }
